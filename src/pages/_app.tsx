79271504c7067f2cec9ca41d7d1789212544f90d
patrickmogul <patrick@easynode.pro>
2023-02-08 21:06:54 -0600

    stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,18 @@ export default function Nextra({
 }: AppProps): ReactElement {
   return (
     <>
+      <script
+        data-name="BMC-Widget"
+        data-cfasync="false"
+        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+        data-id="easynodepro"
+        data-description="Support me on Buy me a coffee!"
+        data-message="Thanks for stopping by! If we help you stake or validate consider helping us make more content by buying us a coffee!"
+        data-color="#2CEA26"
+        data-position="Right"
+        data-x_margin="18"
+        data-y_margin="18"
+      ></script>
       <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </>
